8668f7d3bad32d24ecbbd8b07befb6cba6dc6671
arquivossarah12a <arquivossarah12a@gmail.com>
2025-04-02 05:03:43 +0000

    primeiro card do mobile finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export function App(){
       <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold"/>
     </div>
 
-    <button>Submit</button>
+    <button className="bg-orange w-full py-3 rounded-3xl uppercase text-sm tracking-1">Submit</button>
     </div>
   )
 }
