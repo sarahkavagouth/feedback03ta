29a76dc2d0f83d3acfd9fa2e541c5bd2375bed6a
arquivossarah12a <arquivossarah12a@gmail.com>
2025-04-02 04:13:27 +0000

    primeiro commit do projeto e estrutura do HTML

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import iconStar from "./assets/icon-star.svg"
+
+export function App(){
+  return(
+    <div>
+      <div>
+      <img src={iconStar} alt="" />
+    </div>
+
+    <h1>How did we do?</h1>
+    <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+
+    <div>
+      <input type="button" value={1} />
+      <input type="button" value={2} />
+      <input type="button" value={3} />
+      <input type="button" value={4} />
+      <input type="button" value={5} />
+    </div>
+    
+    <button>Submit</button>
+    </div>
+  )
+}
