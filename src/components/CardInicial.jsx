6d5dcc869493ca6feac12cbb94e69d691ec3e0a2
sarahkavagouth <sarahkavagouth@outlook.com>
2025-04-22 01:31:27 -0300

    mudança da função handleTrocarNota do CardInicial.jsx para o InputButton.jsx

--- src/components/CardInicial.jsx
+++ src/components/CardInicial.jsx
@@ -2,11 +2,7 @@ import iconStar from "../assets/icon-star.svg"
 import { InputButton } from "./InputButton"
 
 export function CardInicial({nota, setNota, setSubmited}){
-    function handleTrocarNota(nota){
-        setNota(nota)
-    
-      }
-
+   
       function handleSubmit() {
         if(nota !== 0){
           setSubmited(true)
@@ -23,14 +19,15 @@ export function CardInicial({nota, setNota, setSubmited}){
       </div>
     
       <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
+
       <p className="text-sm text-light-grey mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
     
       <div className="flex justify-between mb-6">
-        <InputButton valor={1} handleTrocarNota={handleTrocarNota}/>
-        <InputButton valor={2} handleTrocarNota={handleTrocarNota}/>
-        <InputButton valor={3} handleTrocarNota={handleTrocarNota}/>
-        <InputButton valor={4} handleTrocarNota={handleTrocarNota}/>
-        <InputButton valor={5} handleTrocarNota={handleTrocarNota}/>
+        <InputButton valor={1} setNota={setNota} />
+        <InputButton valor={2} setNota={setNota} />
+        <InputButton valor={3} setNota={setNota} />
+        <InputButton valor={4} setNota={setNota} />
+        <InputButton valor={5} setNota={setNota} />
       </div>
     
       <button onClick = {handleSubmit} 
--- src/components/InputButton.jsx
+++ src/components/InputButton.jsx
@@ -1,4 +1,9 @@
-export function InputButton({valor, handleTrocarNota}){
+export function InputButton({valor, setNota}){
+    function handleTrocarNota(nota){
+        setNota(nota)
+    
+      }
+
     return(
         <input type="button" value={valor} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white sm:w-13 sm:h-13 cursor-pointer" onClick={() => handleTrocarNota(valor)}/>
     )
